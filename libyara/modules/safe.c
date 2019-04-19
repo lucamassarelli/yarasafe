55c1aa01b4acf41cf76c08b834ce01b02a750955
Luca Massarelli <massarelli@diag.uniroma1.it>
2019-04-19 11:46:35 +0200

    Update safe.c
    UPDATE LICENSE

--- libyara/modules/safe.c
+++ libyara/modules/safe.c
@@ -1,3 +1,10 @@
+/* SAFE TEAM
+#
+#
+# distributed under license: CC BY-NC-SA 4.0 (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.txt) #
+*/
+
+
 #include <yara/modules.h>
 #include <yara/mem.h>
 #include <python/Python.h>
